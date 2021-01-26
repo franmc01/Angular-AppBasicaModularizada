8ee1996051cf9c5d8f53eec2caede62b42ebc241
francisco-1998 <francmarinc@gmail.com>
2021-01-26 18:17:34 -0500

    Haciendo un TODO

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string,
+  poder: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -7,8 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent {
 
-  Agregar(){
-    console.log('Hey');
+  lista:Personaje[] = [
+    {
+      nombre:'Goku',
+      poder: 100000
+    },
+    {
+      nombre:'Bills',
+      poder: 2000000
+    },
+    {
+      nombre:'Vegeta',
+      poder: 40000
+    },
+    {
+      nombre:'Krilin',
+      poder: 10000
+    }
+  ];
+  nuevoPersonaje: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  Agregar() {
+    if (this.nuevoPersonaje.nombre.trim().length === 0) {
+      return;
+    }
+    console.log(this.nuevoPersonaje);
+    this.lista.push(this.nuevoPersonaje);
+    this.nuevoPersonaje = {
+      nombre: '',
+      poder:0
+    }
+
   }
 
 }
